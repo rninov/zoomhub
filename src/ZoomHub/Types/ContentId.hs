f9902e3e43c1d2dfb1b5db5abe93c9347868b8df
Daniel Gasienica <daniel@gasienica.ch>
2017-01-22 19:07:17 -0800

    Add support for `ContentId` for PostgreSQL

--- src/ZoomHub/Types/ContentId.hs
+++ src/ZoomHub/Types/ContentId.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable    #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 
 module ZoomHub.Types.ContentId
   ( ContentId
@@ -10,7 +13,7 @@ module ZoomHub.Types.ContentId
   , validChars
   ) where
 
-import           Prelude                          hiding (fromInteger)
+import           Prelude                              hiding (fromInteger)
 
 import           Data.Aeson                       (FromJSON, ToJSON,
                                                    genericParseJSON,
@@ -21,6 +24,7 @@ import           Data.List                        (intersperse)
 import           Data.Set                         (Set)
 import qualified Data.Set                         as Set
 import qualified Data.Text                        as T
+import qualified Database.PostgreSQL.Simple.FromField as PGS
 import           Database.SQLite.Simple           (SQLData (SQLText))
 import           Database.SQLite.Simple.FromField (FromField, ResultError (ConversionFailed),
                                                    fromField, returnError)
@@ -30,6 +34,11 @@ import           Database.SQLite.Simple.ToField   (ToField, toField)
 import           GHC.Generics                     (Generic)
 import           Servant                          (FromHttpApiData,
                                                    parseUrlPiece)
+import           Opaleye                              (PGText,
+                                                       QueryRunnerColumnDefault,
+                                                       fieldQueryRunnerColumn,
+                                                       queryRunnerColumnDefault)
+import           Servant                              (FromText, fromText)
 
 
 
@@ -85,3 +94,14 @@ instance FromField ContentId where
       Right r  -> Ok r
       Left _ -> returnError ConversionFailed f "Invalid content ID"
   fromField f = returnError ConversionFailed f "Invalid content ID"
+
+-- PostgreSQL
+instance PGS.FromField ContentId where
+  fromField f mdata = PGS.fromField f mdata >>= parseContentId
+    where
+      parseContentId t = case fromText t of
+        Just r  -> return r
+        Nothing -> PGS.returnError PGS.ConversionFailed f "invalid content ID"
+
+instance QueryRunnerColumnDefault PGText ContentId where
+  queryRunnerColumnDefault = fieldQueryRunnerColumn
