8aa013476042bc7f183f0fe1b746e15b7edf79a2
Daniel Gasienica <daniel@gasienica.ch>
2018-10-15 01:21:27 -0400

    Replace `Integer` with `Int64` in `Content` type

--- src/ZoomHub/Types/Content.hs
+++ src/ZoomHub/Types/Content.hs
@@ -3,46 +3,54 @@ module ZoomHub.Types.Content
   , mkContent
   ) where
 
-import           Data.Text                   (Text)
-import           Data.Time.Clock             (UTCTime)
+import Data.Int (Int64)
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
+import qualified GHC.Generics as GHC
+import qualified Generics.SOP as SOP
 
-import           ZoomHub.Types.ContentId     (ContentId)
-import           ZoomHub.Types.ContentMIME   (ContentMIME)
-import           ZoomHub.Types.ContentState  (ContentState (Initialized))
-import           ZoomHub.Types.ContentType   (ContentType)
-import           ZoomHub.Types.ContentURI    (ContentURI)
-import           ZoomHub.Types.DeepZoomImage (DeepZoomImage)
+import ZoomHub.Types.ContentId (ContentId)
+import ZoomHub.Types.ContentMIME (ContentMIME)
+import ZoomHub.Types.ContentState (ContentState(Initialized))
+import ZoomHub.Types.ContentType (ContentType)
+import ZoomHub.Types.ContentURI (ContentURI)
+import ZoomHub.Types.DeepZoomImage (DeepZoomImage)
 
 -- Content
 data Content = Content
-  { contentId            :: ContentId
-  , contentURL           :: ContentURI
-  , contentType          :: ContentType
-  , contentState         :: ContentState
+  { contentId :: ContentId
+  , contentType :: ContentType
+  , contentURL :: ContentURI
+  , contentState :: ContentState
   , contentInitializedAt :: UTCTime
-  , contentActiveAt      :: Maybe UTCTime
-  , contentCompletedAt   :: Maybe UTCTime
-  , contentMIME          :: Maybe ContentMIME
-  , contentSize          :: Maybe Integer
-  , contentProgress      :: Double
-  , contentNumViews      :: Integer
-  , contentError         :: Maybe Text
-  , contentDZI           :: Maybe DeepZoomImage
-  } deriving (Eq, Show)
+  , contentActiveAt :: Maybe UTCTime
+  , contentCompletedAt :: Maybe UTCTime
+  , contentMIME :: Maybe ContentMIME
+  , contentSize :: Maybe Int64
+  , contentProgress :: Double
+  , contentNumViews :: Int64
+  , contentError :: Maybe Text
+  , contentDZI :: Maybe DeepZoomImage
+  } deriving (Eq, GHC.Generic, Show)
 
 mkContent :: ContentType -> ContentId -> ContentURI -> UTCTime -> Content
-mkContent type_ cId uri initializedAt = Content
-  { contentId = cId
-  , contentURL = uri
-  , contentType = type_
-  , contentState = Initialized
-  , contentInitializedAt = initializedAt
-  , contentActiveAt = Nothing
-  , contentCompletedAt = Nothing
-  , contentMIME = Nothing
-  , contentSize = Nothing
-  , contentProgress = 0.0
-  , contentNumViews = 0
-  , contentError = Nothing
-  , contentDZI = Nothing
-  }
+mkContent type_ cId uri initializedAt =
+  Content
+    { contentId = cId
+    , contentType = type_
+    , contentURL = uri
+    , contentState = Initialized
+    , contentInitializedAt = initializedAt
+    , contentActiveAt = Nothing
+    , contentCompletedAt = Nothing
+    , contentMIME = Nothing
+    , contentSize = Nothing
+    , contentProgress = 0.0
+    , contentNumViews = 0
+    , contentError = Nothing
+    , contentDZI = Nothing
+    }
+
+-- PostgreSQL / Squeal
+instance SOP.Generic Content
+instance SOP.HasDatatypeInfo Content
